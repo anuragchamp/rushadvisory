b125d749b574f06aa92fc12fdde17d6a38f78721
unknown <anuragchhaperwal@gmail.com>
2024-02-27 18:43:06 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./dist/*.html'],
+  theme: {
+    colors:{
+      'blue':'#39BED7',
+      'white': '#fff',
+      'red':'#FF2121',
+      'light-grey':'#EFEFEF',
+      'dark-grey':'#A9A9A9'
+    },
+
+    extend: {},
+  },
+  plugins: [],
+}
+
